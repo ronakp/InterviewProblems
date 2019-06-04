74a3c5f1e106044225473a87457f5b80ef9360df
Ronak Patel <ronakp991@gmail.com>
2019-06-04 18:53:49 -0400

    Stack

--- src/com/ronakp/random/StackProblem1.java
+++ src/com/ronakp/random/StackProblem1.java
@@ -1,4 +1,27 @@
 package com.ronakp.random;
 
+import com.ronakp.random.helper.Node;
+
 public class StackProblem1 {
+    Node head;
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public int pop() {
+        int data = head.data;
+        head = head.next;
+        return data;
+    }
+
+    public int peek() {
+        return head.data;
+    }
+
+    public boolean isEmpty() {
+        return head == null;
+    }
 }
