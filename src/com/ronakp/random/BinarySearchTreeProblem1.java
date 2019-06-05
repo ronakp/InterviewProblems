385e53d93bbafb1e746d282d32412c1121ab221b
Ronak Patel <ronakp991@gmail.com>
2019-06-05 15:51:19 -0400

    Tree

---
+++ src/com/ronakp/random/BinarySearchTreeProblem1.java
@@ -0,0 +1,31 @@
+package com.ronakp.random;
+
+import com.ronakp.random.helper.TreeNode;
+
+public class BinarySearchTreeProblem1 {
+    TreeNode root;
+
+    public BinarySearchTreeProblem1(int data) {
+        root = new TreeNode(data);
+    }
+
+    public void insert(int data) {
+
+    }
+
+    public void delete(int data) {
+
+    }
+
+    public void InOrder() {
+
+    }
+
+    public void PreOrder() {
+
+    }
+
+    public void PostOrder() {
+
+    }
+}
---
+++ src/com/ronakp/random/helper/TreeNode.java
@@ -0,0 +1,12 @@
+package com.ronakp.random.helper;
+
+public class TreeNode {
+    public int data;
+    public TreeNode right;
+    public TreeNode left;
+
+    public TreeNode(int data) {
+        this.data = data;
+        right = left = null;
+    }
+}
