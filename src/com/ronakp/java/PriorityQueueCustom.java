923fd621329e655507ab66c370e4e6277105e064
Ronak Patel <ronakp991@gmail.com>
2021-07-13 21:44:01 -0400

    Priority Queue

--- src/com/ronakp/java/PriorityQueueCustom.java
+++ src/com/ronakp/java/PriorityQueueCustom.java
@@ -1,5 +1,26 @@
 package com.ronakp.java;
 
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
 public class PriorityQueueCustom {
-    
+
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(78);
+        pq.add(30);
+        pq.add(89);
+
+        Iterator<Integer> it = pq.iterator();
+        
+        while(it.hasNext()) {
+            System.out.println(it.next());
+        }
+
+        System.out.println(pq); //[30, 78, 89]
+
+        System.out.println(pq.peek()); //30
+        System.out.println(pq.poll()); //30
+    }
 }
