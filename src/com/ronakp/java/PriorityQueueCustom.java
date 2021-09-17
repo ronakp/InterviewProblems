10082ea69bde82df7493b34dd350eeff0f002b68
Ronak Patel <ronakp991@gmail.com>
2021-09-16 20:20:59 -0400

    PQ

--- src/com/ronakp/java/PriorityQueueCustom.java
+++ src/com/ronakp/java/PriorityQueueCustom.java
@@ -27,7 +27,7 @@ public class PriorityQueueCustom {
         Comparator<Integer> comparator = new Comparator<Integer>() {
 
             @Override
-            public int compare(int n1, int n2) {
+            public int compare(Integer n1, Integer n2) {
                 return n1<=n2 ? n2 : n1;
             }
         };
